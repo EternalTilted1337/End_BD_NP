368c0621026ff8d8be001943b3ec099f1fa46a78
Administrator <anitikrev@gmail.com>
2023-08-15 20:24:48 +0300

    Шо тут не так?

--- BD_NP/views.py
+++ BD_NP/views.py
@@ -9,11 +9,27 @@ class NewsListView(ListView):
     template_name = 'news_list.html'
     context_object_name = 'object'
     queryset = Post.objects.order_by('-id')
+    paginate_by = 10 # вот так мы можем указать количество записей на странице
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['post_id'] = self.object_list.first().id
-        return context
+def get_queryset(self):
+   # Получаем обычный запрос
+   queryset = super().get_queryset()
+   # Используем наш класс фильтрации.
+   # self.request.GET содержит объект QueryDict, который мы рассматривали
+   # в этом юните ранее.
+   # Сохраняем нашу фильтрацию в объекте класса,
+   # чтобы потом добавить в контекст и использовать в шаблоне.
+   self.filterset = Post(self.request.GET, queryset)
+   # Возвращаем из функции отфильтрованный список товаров
+   return self.filterset.qs
+
+
+def get_context_data(self, **kwargs):
+    context = super().get_context_data(**kwargs)
+    # Добавляем в контекст объект фильтрации.
+    context['filterset'] = self.filterset
+    return context
+
 
 class NewsDetailView(DetailView):
     model = Post
@@ -27,10 +43,10 @@ def news_list(request):
     total_news_count = articles.count()
     return render(request, 'news_list.html', {'articles': articles, 'total_news_count': total_news_count})
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['post'] = self.object
-        return context
+def get_context_data(self, **kwargs):
+    context = super().get_context_data(**kwargs)
+    context['post'] = self.object
+    return context
 
-from django.shortcuts import render
+
 
--- NewsPaper/settings.py
+++ NewsPaper/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'BD_NP',
+    'django_filters'
 ]
 
 
